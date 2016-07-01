1e7f29ea7e096a55f10eb7c5f03e5eb7ca241a48
Addison Higham <ahigham@instructure.com>
2016-07-01 15:48:32 -0600

    place override path inside file call
    Change-Id: I50526e75a1f0ce6476af5f99d91b4b7c7157cab7

--- modules/dcos_asg/main.tf
+++ modules/dcos_asg/main.tf
@@ -54,7 +54,7 @@ resource "aws_autoscaling_group" "dcos_asg" {
 }
 
 resource "template_file" "user_data" {
-  template = "${coalesce(var.cloud_config_path, file("${path.module}/files/user_data/cloud-config.yaml.tpl"))}"
+  template = "${file(coalesce(var.cloud_config_path, format("%s/files/user_data/cloud-config.yaml.tpl", path.module)))}"
   vars {
     bootstrap_url = "${var.dcos_install_url}"
     role = "${var.dcos_role}"
