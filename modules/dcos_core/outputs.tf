eb2f7e7e7c58bec1780ef8cc94193e493021b8f9
Addison Higham <ahigham@instructure.com>
2016-07-02 16:19:34 -0600

    add more outputs
    Change-Id: Id1c6dcb9dc533f37944fb26762067ebaa85f27dd

---
+++ modules/dcos_core/outputs.tf
@@ -0,0 +1,87 @@
+output "default_security_group" {
+  value = "${module.default_sec_group.id}"
+}
+output "master_internal_elb_dns" {
+  value = "${module.master_internal_lb.elb_dns}"
+}
+output "master_internal_elb" {
+  value = "${module.master_internal_lb.elb}"
+}
+output "master_internal_sec_group" {
+  value = "${module.master_internal_lb.sec_group}"
+}
+output "dcos_install_url" {
+  value = "${module.dcos_bootstrap.dcos_install_url}"
+}
+output "master_launch_config" {
+  value = "${module.masters.launch_config}"
+}
+output "master_asg" {
+  value = "${module.masters.asg}"
+}
+output "master_asg_sec_group" {
+  value = "${module.masters.asg_sec_group}"
+}
+output "master_role_arn" {
+  value = "${module.masters.role_arn}"
+}
+output "master_role_id" {
+  value = "${module.masters.role_id}"
+}
+output "master_public_lb_dns" {
+  value = "${module.masters.public_lb_dns}"
+}
+output "master_public_lb" {
+  value = "${module.masters.public_lb}"
+}
+output "master_public_lb_sec_group" {
+  value = "${module.masters.public_lb_sec_group}"
+}
+output "agent_launch_config" {
+  value = "${module.agents.launch_config}"
+}
+output "agent_asg" {
+  value = "${module.agents.asg}"
+}
+output "agent_asg_sec_group" {
+  value = "${module.agents.asg_sec_group}"
+}
+output "agent_role_arn" {
+  value = "${module.agents.role_arn}"
+}
+output "agent_role_id" {
+  value = "${module.agents.role_id}"
+}
+output "agent_lb_dns" {
+  value = "${module.agents.lb_dns}"
+}
+output "agent_lb" {
+  value = "${module.agents.lb}"
+}
+output "agent_lb_sec_group" {
+  value = "${module.agents.lb_sec_group}"
+}
+output "public_agent_launch_config" {
+  value = "${module.public_agents.launch_config}"
+}
+output "public_agent_asg" {
+  value = "${module.public_agents.asg}"
+}
+output "public_agent_asg_sec_group" {
+  value = "${module.public_agents.asg_sec_group}"
+}
+output "public_agent_role_arn" {
+  value = "${module.public_agents.role_arn}"
+}
+output "public_agent_role_id" {
+  value = "${module.public_agents.role_id}"
+}
+output "public_agent_lb_dns" {
+  value = "${module.public_agents.lb_dns}"
+}
+output "public_agent_lb" {
+  value = "${module.public_agents.lb}"
+}
+output "public_agent_lb_sec_group" {
+  value = "${module.public_agents.lb_sec_group}"
+}
